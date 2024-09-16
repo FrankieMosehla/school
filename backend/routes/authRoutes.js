10b21da26cbc95b1d6febd2cd3f90171a3055520
= <=>
2024-09-16 16:34:21 +0200

    init

--- backend/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const sign_up = require('../controllers/authControler');
-
-const routes = Router();
-
-routes.post('/api/signup', sign_up);
-
-module.exports = routes
